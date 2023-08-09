671d462705f6a2d64f1196785c21d4145f453f04
Rob Pickerill <r.pickerill@gmail.com>
2023-08-09 18:29:21 +0100

    add python equilivant

---
+++ getmetrics.py
@@ -0,0 +1,29 @@
+import boto3
+
+client = boto3.client('cloudwatch')
+
+response = client.get_metric_data(
+    MetricDataQueries=[
+        {
+            'Id': 'e1',
+            'MetricStat': {
+                'Metric': {
+                    'Namespace': 'AWS/Usage',
+                    'MetricName': 'ConcurrentExecutions',
+                },
+                'Period': 60,
+                'Stat': 'Maximum',
+            },
+            'ReturnData': False,
+        },
+        {
+            'Id': 'e2',
+            'Expression': 'SERVICE_QUOTA(e1)',
+            'ReturnData': True,
+        }
+    ],
+    StartTime='2023-08-01T00:00:00Z',
+    EndTime='2023-08-02T00:00:00Z',
+)
+
+print(response)
